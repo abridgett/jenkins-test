1f5ff49494e8fcd285080dd5414054616de5667c
Adrian Bridgett <adrian@smop.co.uk>
2015-07-09 10:05:39 +0100

    added testing C code received some cleanup

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 /* Hello World program */
 
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(int argc, char *argv[])
 {
@@ -13,4 +14,5 @@ int main(int argc, char *argv[])
       printf("Hello %s!\n", argv[i]);
     }
   }
+  exit(0);
 }
