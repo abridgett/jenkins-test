8cb6ffaf558eee713b2c79cf9e095b7f338e07fb
Adrian Bridgett <adrian@smop.co.uk>
2015-07-09 09:33:52 +0100

    allow arguments to hello

--- hello.c
+++ hello.c
@@ -2,8 +2,15 @@
 
 #include<stdio.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
+  int i;
+  if (argc == 1) {
     printf("Hello World!\n");
-
+  } else {
+    for(i=1; i<argc; i++)
+    {
+      printf("Hello %s!\n", argv[i]);
+    }
+  }
 }
